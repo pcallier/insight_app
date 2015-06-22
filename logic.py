1d4b5a103b8aa937399b1f68ba64d71b6fc81bfe
Patrick Callier <pat.r.callier@gmail.com>
2015-06-22 10:19:07 -0700

    Changed db parameters for remote

--- logic.py
+++ logic.py
@@ -6,11 +6,36 @@ import pickle
 import numpy as np
 import tokenization as tknz
 import datetime
+import psycopg2 as mdb
+import logging
+import pandas as pd
+logging.basicConfig(level=logging.DEBUG)
 
+
+dbuser="ubuntu"
+
+def get_random_users(prediction=True, n=5):
+    con = mdb.connect(dbname="tweets",user=dbuser)
+    with con.cursor() as cur:
+        cur.execute(u"SELECT name, screen_name, profile_pic_url, "
+        "prediction FROM users WHERE prediction IS NOT NULL AND name "
+        "IS NOT NULL AND screen_name IS NOT NULL "
+        "AND profile_pic_url IS NOT NULL")
+        data = np.array(cur.fetchall())
+        print data.shape
+        print data[[0,1,2],3]
+        data = data[data[:,3]==str(prediction), :]
+        print data.shape
+        indices = np.random.random_integers(0, data.shape[0], n)
+        print indices
+        print data[indices,:].shape
+        return [ { 'name': r[0].decode('utf-8') ,'screen_name': r[1].decode('utf-8'), 'profile_image_url': r[2] } for r in data[indices] if r is not None ]
 
 def get_tweets_by_user(screen_name):
     api = twapi.get_api()
+    logging.debug(screen_name)
     user_tweets = api.user_timeline(screen_name=screen_name, count=4)
+    logging.debug("Number of tweets retrieved: {}".format(len(user_tweets)))
     return user_tweets
     
 
@@ -19,29 +44,41 @@ def vectorize_tweeter(screen_name_or_id, tweets, query_date=datetime.datetime.no
     their user information and timeline. Returns tuple of untransformed
     features (in a dict) and transformed features (in a numpy array"""
     
-    features = { 'age' : None, 'friends_count': None, 'followers_count': None }
+    features = { 'age' : None, 'friends_count': None, 'followers_count': None,
+    'name': None, 'profile_image_url': None }
     
     try:
         api = twapi.get_api()
         user_corpus = [tweet.text for tweet in tweets]
-        scaler = pickle.load(file("scaler.pickle","r"))
+        scaler = pickle.load(file("app/static/models/scaler.pickle","r"))
         user = api.get_user(screen_name_or_id)
-        age = datetime.user.created_at
+        now = pd.to_datetime(datetime.datetime.now())
+        age = now - user.created_at
         if age.days > 365:
             user_age = "%0.1f years" % (float(age.days) / 365.)
         elif age.days < 365:
             user_age = "%d months" % (age.days / 30)
+        
         features = { 'age' :  user_age,
                      'friends_count' : user.friends_count,
-                     'followers_count' : user.followers_count }
-        scaled_features = scaler.transform(np.array(features['followers_count'], 
+                     'followers_count' : user.followers_count,
+                     'name': user.name,
+                     'profile_image_url': user.profile_image_url.replace(u"_normal", u"") }
+        age = pd.Series(age).astype(np.int64).values[0]
+        
+        to_transform=np.array([features['followers_count'], 
                                     features['friends_count'], 
-                                    features['age']))
+                                    age]).T
+        logging.debug(to_transform)
+        scaled_features = np.array(scaler.transform(to_transform))
+    except:
+        logging.warning("Exception: ", exc_info=True)
+    
     finally:
         return (features, scaled_features)
     
 def load_model(model_path=None):
     if model_path is None:
-        model_path="svm.pickle"
+        model_path="app/static/models/svm1434693300.pkl"
     model = pickle.load(file(model_path, "r"))
     return model
--- views.py
+++ views.py
@@ -1,39 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import sys
+import os
 from flask import render_template , request, url_for, redirect
 from app import app
-import psycopg2 as mdb
-from logic import vectorize_tweeter, load_model
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-db= mdb.connect("dbname=tweets user=patrick")
+import psycopg2 as mdb
+from logic import vectorize_tweeter, load_model, get_tweets_by_user, \
+    get_random_users
+
+dbuser="ubuntu"
+db= mdb.connect(dbname="tweets", user=dbuser)
 svm = load_model()  
     
 @app.route('/')
 @app.route('/index')
 def index():
-    return redirect(url_for('twitter_user_view'))
+    return redirect(url_for('app_dashboard'))
 
 @app.route("/app/")
-@app.route("/app/<screen_name>")
+def app_dashboard():
+    random_users = get_random_users()
+    return render_template('dashboard.html', users=random_users)
+
+@app.route("/user", methods=["GET"])
+def redirect_to_user():
+    screen_name = request.args.get('screen_name')
+    logging.debug(screen_name)
+    return redirect(url_for('twitter_user_view', screen_name=screen_name))
+
+@app.route("/user/<screen_name>")
 def twitter_user_view(screen_name=None):
+    
     if screen_name == "":
         screen_name=None
     try:
-        features, tweets = vectorize_tweeter(screen_name)
-        #on_road=svm.predict(features)
+        tweets = get_tweets_by_user(screen_name)
+        logging.debug(screen_name)
+        features, scaled_features = vectorize_tweeter(screen_name, tweets)
+        will_churn=svm.predict(scaled_features)
         error_txt = None
-    except TypeError:
-        tweets = None
-        on_road = None
-        error_txt = "Type error"
+
     except Exception as e:
-        tweets = None
-        on_road = None
-        error_txt =  str(e)
+        error_txt="Error"
+        logging.debug("Error: ", exc_info=True)
+        raise
         
-    return render_template('twitter-user.html', 
+    return render_template('twitter-user.html',
+                           twitter_name=features['name'],
                            screen_name=screen_name,
-                           tweets=tweets,
-                           on_road=False,
+                           tweet_list=tweets,
+                           user=features,
+                           will_churn=will_churn,
                            error_txt=error_txt)
+                           
