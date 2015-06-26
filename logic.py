d17ee0c7ceb8b294ae5e9a6d0ecd87c35c21d070
Patrick Callier <pat.r.callier@gmail.com>
2015-06-26 16:53:03 -0700

    Load new model

--- logic.py
+++ logic.py
@@ -12,37 +12,6 @@ import pandas as pd
 import sklearn
 logging.basicConfig(level=logging.DEBUG)
 
-class LogScaler(sklearn.base.TransformerMixin):
-    def __init__(self, base = 10, colnames=None):
-        self.base = base
-        self.colnames = colnames
-        pass
-    
-    def fit(self, X, y=None, **fit_params):
-        return self
-    
-    def transform(self, X, y=None, copy=None, correction=0.01):
-        """Log transform X or columns in X identified by
-        colnames kwarg
-        
-        Arguments
-        X         dataframe or numpy array to trasnform
-        colnames  names of columns to transform
-        base      base of log to transform, (10 if not given)
-        """
-        
-        try:
-            new_X_cols = np.log(X.loc[:,self.colnames].replace(0,correction)) / \
-                                            np.log(self.base)
-            new_X = pd.concat((X.loc[:,[c for c in X.columns if c not in self.colnames]],
-                               new_X_cols),axis=1)[X.columns]
-            #print new_X.head()
-        except AttributeError, ValueError:
-            new_X = np.log([np.max(x,correction) for x in X])/np.log(self.base)
-        
-        return new_X
-
-
 dbuser="ubuntu"
 
 def get_random_users(prediction=True, n=5):
@@ -114,6 +83,7 @@ def vectorize_tweeter(screen_name_or_id, tweets, query_date=datetime.datetime.no
 def load_model(model_path=None):
     if model_path is None:
         #model_path="app/static/models/svm1434693300.pkl"
-        model_path="app/static/models/logreg_1435302860.pkl"
+        #model_path="app/static/models/logreg_1435302860.pkl"
+        model_path="app/static/models/logreg_1435362548.pkl"
     model = pickle.load(file(model_path, "r"))
     return model
